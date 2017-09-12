e732caa3a0fc9099ee583aa7ec804e29d8782bb8
dmaharjan <dilipmaharjann@gmail.com>
2017-09-12 15:43:49 -0400

    custom middleware added

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ var bodyParser=require('body-parser');
 var path=require('path');
 
 var app=express();
+
+//custom middleware
+
+var logger=(req,res,next)=>{
+console.log('Logging...');
+next();
+}
+
+//need to use logger to be able to use it
+app.use(logger);
+
+//making get request
 app.get('/',(req,res)=>{
   res.send('Node Express App started at port 2017');
 });
+
+//listening to port 2017
 app.listen(2017,()=>{
   console.log('Node express app started on 2017');
 })
